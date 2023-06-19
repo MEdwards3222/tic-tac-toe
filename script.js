3023b65abc2823301cbcf02815b28910d331fbab
MEdwards3222 <medwards3222@gmail.com>
2023-06-19 16:23:02 +0000

    Create objects and functions to build

---
+++ script.js
@@ -0,0 +1,41 @@
+const Grid = (() => {   //module pattern - only one grid
+    let row1 = ["", "", ""];
+    let row2 = ["", "", ""];
+    let row3 = ["", "", ""];
+})();
+
+const Player = (name) => { //factory function - multiple players
+
+    let sign = "";
+    const setSign = (reqSign) => {
+        sign = sign;
+        return sign;
+    }
+
+    const getSign = () => console.log(sign);
+
+    return {setSign, getSign};
+}
+
+const Game = (() => { //module pattern - only one grid
+
+    const playRound = () => {
+        //TODO
+    }
+
+    const checkWin = () => {
+        //TODO
+    }
+
+    const declareWinner = () => {
+        //TODO
+    }
+
+    const getScore = () => {
+        //TODO
+    }
+
+    const setScore = () => {
+        //TODO
+    }
+})();
